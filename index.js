9522d3a91c98881c2a10ccc64b54d5601d0284f2
Aaron Parecki <aaron@parecki.com>
2013-12-11 10:33:27 -0800

    update references to css/js files

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var eejs = require('ep_etherpad-lite/node/eejs')
   , padManager = require('ep_etherpad-lite/node/db/PadManager')
-  , api = require('ep_etherpad-lite/node/db/API');
+  , api = require('ep_etherpad-lite/node/db/API')
+  , express = require('ep_etherpad-lite/node_modules/express');
 
 exports.expressCreateServer = function (hook_name, args, cb) {
   args.app.get('/list', function(req, res) {
@@ -13,9 +14,12 @@ exports.expressCreateServer = function (hook_name, args, cb) {
       cb();
     });
   });
+
+  args.app.use('/list/static', express.static(__dirname + '/static'))
 }
 
 exports.indexWrapper = function (hook_name, args, cb) {
   args.content = args.content + '<br><br><div style="text-align:center;"><a href="/list">All Pads</a></div>';
   return cb();
 }
+
