63ff8f91c400f561cf032279184d1a70bc0af788
Richard Hansen <rhansen@rhansen.org>
2020-10-12 13:51:11 -0400

    New `q` query parameter that updates to match the search box
    This makes it possible to bookmark a particular search.

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,7 @@
 /* global $ */
 
 $(() => {
-  const searchBox = $('input');
-  const filter = () => {
-    const query = searchBox.val();
+  const filter = (query) => {
     const list = $('ul');
     if (!query.length) {
       list.removeClass('filtering');
@@ -18,6 +16,15 @@ $(() => {
       });
     }
   };
-  filter();
-  searchBox.on('input', filter);
+  const queryParams = new URLSearchParams(window.location.search);
+  const searchBox = $('input');
+  const q = queryParams.has('q') ? queryParams.get('q') : searchBox.val();
+  filter(q);
+  searchBox.val(q);
+  searchBox.on('input', () => {
+    const val = searchBox.val();
+    queryParams.set('q', val);
+    history.replaceState(null, '', `${window.location.pathname}?${queryParams.toString()}`);
+    filter(val);
+  });
 });
